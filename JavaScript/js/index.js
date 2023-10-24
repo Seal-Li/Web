b46326d94da8aafb83b6bb2c278cb2a75376d85a
hbli <lihaibao98@163.com>
2023-10-24 11:30:07 +0800

    [add] add js garbage collection

--- JavaScript/js/index.js
+++ JavaScript/js/index.js
@@ -18,12 +18,12 @@ document.getElementById('btn').onclick = function(){
     // console.log(str);
 
     // bool 值 true false,多用于条件判断
-    var str1 = 'Hello World';
-    var str2 = "Hello World";
+    // var str1 = 'Hello World';
+    // var str2 = "Hello World";
     // console.log(str1 == str2);
 
     // null 空值,多用于变量定义时
-    var arr = null;
+    // var arr = null;
     // console.log(arr);
 
     // 基本数据类型
@@ -33,18 +33,67 @@ document.getElementById('btn').onclick = function(){
     // 用于存放多个相同意义的值
     // 数组中的值的类型可以不一致
     // 数组不属于js的基本数据类型,而是js对象
-    arr = ["1", 2, str1]
+    // arr = ["1", 2, str1]
     // console.log(arr);
     // console.log(arr[2]);
 
     // 对象
-    var obj = {
-        name: "name",
-        age: 18,
-        sex: "男",
-        hobby: ["sing", "dance", "rap"],
-        isStudent: true,
-        tel: 12345678901
-    }
-    console.log(obj);
+    // var obj = {
+    //     name: "name",
+    //     age: 18,
+    //     sex: "男",
+    //     hobby: ["sing", "dance", "rap"],
+    //     isStudent: true,
+    //     tel: 12345678901
+    // }
+    // console.log(obj);
+    // console.log(obj.name);
+    
+    // js中的数量类型可以相互转换
+    // 任意类型的值转化为bool值都为true,仅有几个例外
+    // 0, null, '', undefined
+    // console.log(Boolean(0));
+    // console.log(Boolean(null));
+    // console.log(Boolean(''));
+    // console.log(Boolean(undefined));
+
+    // 类型检测
+    // var str = "Hello World";
+    // console.log(typeof str);
+    // str = 123;
+    // console.log(typeof str);
+
+    // 隐式转换
+    // 如果计算两侧有字符串,则转换为字符串(字符串仅限于+操作)
+    // 非+两侧进行运算时,两侧均转为数值类型
+    // NaN与任何数值任何类型运算都转换为NaN
+
+    // var num = 10;
+    // var num1 = 10.5;
+    // console.log(num + num1);
+
+    // var str2 = "10";
+    // var str3 = "10.5";
+    // console.log(str2 + str3);
+
+    // var num = 10;
+    // var str = "abc";
+    // console.log(num + str);
+
+    // console.log(num - str)
+    
+    // 引用数据类型-栈
+    // 程序运行消耗时间,运行时消耗运行内存
+    // 堆栈存储结构: 上下文 堆 栈
+    // | 上下文     | 栈: 存储小块数据(固定)  | 堆: 存储大块数据(弹性) |
+    // |  a->0001   |   0001                |    10                 |
+    // |  PI        |   0002                |  3.14                 |
+    // |            |   0003                |  3.15                 |
+    // |  arr->0004 |   0004                |  hash_value [1,2,3]   |
+
+    const arr = [1, 2, 3];
+    console.log(arr);
+    arr[0] = 10;
+    console.log(arr);
+
 }
