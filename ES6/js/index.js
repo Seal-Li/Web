7b221e10dd0ccbf09df2a7ded0bea0003564da04
hbli <lihaibao98@163.com>
2023-10-26 16:26:45 +0800

    [add] add promise op

--- ES6/js/index.js
+++ ES6/js/index.js
@@ -88,13 +88,75 @@
 // };
 
 // 箭头函数适合与this无关的回溯，定时器，数组方法回调
-console.log(this);
-let box = document.querySelector('.box');
-let that = this;
-box.onclick = function() {
-    console.log(that);
-    setTimeout(() => {
-        console.log(this);
-        box.setAttribute("class", "box bg_green");
-    }, 1000);
-}
+// console.log(this);
+// let box = document.querySelector('.box');
+// let that = this;
+// box.onclick = function() {
+//     console.log(that);
+//     setTimeout(() => {
+//         console.log(this);
+//         box.setAttribute("class", "box bg_green");
+//     }, 1000);
+// }
+
+// promise
+// 有属性是状态，可以通过resolve，reject调整状态
+// pending 异步执行中
+// fulfilled 成功
+// rejected 失败
+// 如果成功了，触发成功后的后续操作；失败亦然
+
+// let p = new Promise((resolve, reject) => {
+//     // let data = {
+//     //     status: 1,
+//     //     message: "成功"
+//     // }
+//     let data = {
+//         status: 0,
+//         message: "失败"
+//     }
+//     if (data.status) {
+//         resolve(data);
+//     } else {
+//         reject(data);
+//     }
+// });
+
+// console.log(p);
+
+// p.then((data) => {
+//     // promise 成功后触发
+//     console.log(data);
+//     return data;
+// }, (data) => {
+//     // promise 失败后触发
+//     console.log(data);
+//     return data;
+// });
+
+// 接口1 接口2 接口3
+// 三个接口必须顺序执行，接口1的结果是接口2的参数，接口2的结果是接口3的参数
+// new Promise((resolve, reject) => {
+//     // 接口1
+//     resolve(1);
+// }).then((data) => {
+//     // 接口2
+//     console.log(data);
+//     return new Promise((resolve, reject) => {
+//         resolve(2);
+//     });
+// }).then((data) => {
+//     // 接口3
+//     console.log(data);
+//     return new Promise((resolve, reject) => {
+//         resolve(3);
+//         console.log(3);
+//     });
+// });
+
+// catch 专用于捕捉失败
+
+// let p = Promise.resolve("success");
+// console.log(p);
+// let p1 = Promise.reject("failed");
+// console.log(p1);
