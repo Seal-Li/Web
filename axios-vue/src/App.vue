b6476999d04f7d6b073ca2deb151c21a9949d56c
hbli <lihaibao98@163.com>
2023-11-03 09:50:33 +0800

    [Init] initial axios-vue

---
+++ axios-vue/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+    <MyAxios> </MyAxios>
+  </div>
+</template>
+
+<script>
+import MyAxios from './components/MyAxios.vue'
+
+export default {
+  name: 'App',
+  components: {
+    MyAxios,
+  },
+};
+</script>
+
+<style>
+#app {
+}
+</style>
---
+++ axios-vue/src/components/MyAxios.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="myaxios">
+        <h2>MyAxios页面</h2>
+        <button @click="submit"> 发送Axios请求</button>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "MyAxios",
+    methods: {
+        submit() {
+            console.log("发送Axios请求")
+        }
+    }
+};
+</script>
+
+<style scoped>
+.myaxios {
+    
+}
+</style>
